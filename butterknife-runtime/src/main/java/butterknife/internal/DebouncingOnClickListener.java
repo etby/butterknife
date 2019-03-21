fdbe7dc54fb816e216fef25c6b13138c37e89a83
Etby Gao <etby.org@gmail.com>
2019-03-21 23:07:09 +0800

    Change: Next OnClick call after delay time.

--- butterknife-runtime/src/main/java/butterknife/internal/DebouncingOnClickListener.java
+++ butterknife-runtime/src/main/java/butterknife/internal/DebouncingOnClickListener.java
@@ -9,15 +9,43 @@ import android.view.View;
 public abstract class DebouncingOnClickListener implements View.OnClickListener {
   static boolean enabled = true;
 
+  public static final long DEFAULT_DELAY_TIME = 200;
+  /**
+   * Interval between two clicks.
+   */
+  private static long mDelayTime = DEFAULT_DELAY_TIME;
+  /**
+   * Last Clicked Time
+   */
+  private long mLastTime = 0L;
+
   private static final Runnable ENABLE_AGAIN = () -> enabled = true;
 
   @Override public final void onClick(View v) {
     if (enabled) {
       enabled = false;
       v.post(ENABLE_AGAIN);
-      doClick(v);
+      performClick(v);
     }
   }
 
   public abstract void doClick(View v);
+
+  /**
+   * Call doClick over delay time.
+   */
+  private void performClick(View v) {
+    if (System.currentTimeMillis() - mLastTime > mDelayTime) {
+      doClick(v);
+    }
+    mLastTime = System.currentTimeMillis();
+  }
+
+  /**
+   * Set time of between two clicks.
+   * @param delayTime MilliSeconds
+   */
+  public static void setDelayTime(long delayTime) {
+    mDelayTime = delayTime;
+  }
 }
